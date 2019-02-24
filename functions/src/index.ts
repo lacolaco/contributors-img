7fb939cdcf2a409d15b761de11b1e83c6bc91ccb
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-02-24 18:34:26 +0900

    initial

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions';
+import * as puppeteer from 'puppeteer';
+
+async function createScreenshot() {
+  const browser = await puppeteer.launch({});
+
+  const page = await browser.newPage();
+
+  await page.goto('https://github.com/angular/angular-ja/graphs/contributors');
+
+  await page.waitForResponse(
+    'https://github.com/angular/angular-ja/graphs/contributors-data',
+  );
+  const screenshotTarget = await page.waitForSelector('#contributors');
+
+  const screenshot = await screenshotTarget.screenshot({ type: 'png' });
+  return await browser.close().then(() => screenshot);
+}
+
+export const createContributorsImage = functions.https.onRequest(
+  (request, response) => {
+    createScreenshot()
+      .then(image => {
+        response.setHeader('Content-Type', 'image/png');
+        response.status(200).send(image);
+      })
+      .catch(err => {
+        console.error(err);
+        response.status(500).send(err);
+      });
+  },
+);
