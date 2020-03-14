72697893c4d1c31b6ce5444387daa7ceec491620
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-03-14 16:55:26 +0900

    refactor: separate sub functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,21 +12,16 @@ const octokit = new Octokit({
 const withCors = cors({ origin: true });
 
 const bucket = admin.storage().bucket();
-const isDebug = process.env.NODE_ENV !== 'production';
 
 function generateCacheId(repository: string) {
   return `image-cache--${repository.replace('/', '-')}`;
 }
 
 async function renderContributorsImage(repository: string): Promise<Buffer> {
-  const browser = await puppeteer.launch(
-    isDebug
-      ? {}
-      : {
-          headless: true,
-          args: ['--no-sandbox'],
-        },
-  );
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setViewport({
     width: 1048,
@@ -34,48 +29,14 @@ async function renderContributorsImage(repository: string): Promise<Buffer> {
   });
 
   console.log(`renderContributorsImage--Go to Page`);
-  await page.goto(`https://contributors-img.firebaseapp.com?repo=${repository}`);
+  await page.goto(`https://contributors-img.firebaseapp.com?repo=${repository}`, { waitUntil: 'networkidle0' });
 
   console.log(`renderContributorsImage--Wait for selector`);
-  const screenshotTarget = await page.waitForSelector('#contributors', {
-    timeout: 0,
-  });
-
-  console.log(`renderContributorsImage--Wait for network`);
-  try {
-    await page.waitForResponse(() => true, {
-      timeout: 10,
-    });
-  } catch (err) {
-    console.warn(err);
-  }
+  const screenshotTarget = await page.waitForSelector('#contributors', { timeout: 0 });
 
   console.log(`renderContributorsImage--Wait for screenshot`);
-  const screenshot = await screenshotTarget.screenshot({
-    type: 'png',
-    omitBackground: true,
-  });
+  const screenshot = await screenshotTarget.screenshot({ type: 'png', omitBackground: true });
   return await browser.close().then(() => screenshot);
-}
-
-async function _createContributorsImage(repository: string): Promise<Buffer> {
-  const cacheId = generateCacheId(repository);
-  const cacheFile = bucket.file(cacheId);
-
-  console.log(`Look for a cache...`);
-  if (await cacheFile.exists().then(data => data[0])) {
-    console.log(`Return from the cache`);
-    return cacheFile.download().then(data => data[0]);
-  }
-
-  console.log(`Render an image`);
-  const image = await renderContributorsImage(repository);
-
-  console.log(`Save new cache`);
-  await cacheFile.save(image, {});
-
-  console.log(`Return rendered image`);
-  return image;
 }
 
 export const createContributorsImage = functions
@@ -83,7 +44,7 @@ export const createContributorsImage = functions
     timeoutSeconds: 60,
     memory: '1GB',
   })
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
     const repoParam = request.query['repo'];
 
     if (!repoParam || typeof repoParam !== 'string') {
@@ -91,15 +52,55 @@ export const createContributorsImage = functions
       return;
     }
 
-    _createContributorsImage(repoParam)
-      .then(image => {
-        response.setHeader('Content-Type', 'image/png');
-        response.status(200).send(image);
-      })
-      .catch(err => {
-        console.error(err);
-        response.status(500).send(err.toString());
+    const cacheId = generateCacheId(repoParam);
+    const useCache = request.hostname !== 'localhost';
+
+    async function readFile(filename: string): Promise<Buffer | null> {
+      console.log(`readFile: ${filename}`);
+      const file = bucket.file(filename);
+      return await file.exists().then(([exists]) => {
+        if (exists) {
+          return file.download({}).then(([data]) => data);
+        }
+        return null;
       });
+    }
+
+    async function writeFile(filename: string, file: Buffer): Promise<void> {
+      console.log(`writeFile: ${filename}`);
+      await bucket.file(filename).save(file, {
+        public: true,
+      });
+    }
+
+    const createImage = async () => {
+      if (useCache) {
+        console.debug('restore cache');
+        const cache = await readFile(cacheId);
+        if (cache) {
+          console.debug('cache is found');
+          return cache;
+        }
+      }
+      console.log(`Render an image`);
+      return renderContributorsImage(repoParam);
+    };
+
+    try {
+      const image = await createImage();
+      if (useCache) {
+        console.debug('save cache');
+        await writeFile(cacheId, image);
+      }
+      response
+        .header('Content-Type', 'image/png')
+        .header('Cache-Control', 'max-age=0, no-cache')
+        .status(200)
+        .send(image);
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error);
+    }
   });
 
 export const getContributors = functions.https.onRequest((request, response) => {
