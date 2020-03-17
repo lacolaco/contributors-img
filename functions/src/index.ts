f70131e815839962f5daedd140b73883eeae6590
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-03-17 22:06:23 +0900

    save contributors json cache

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
-import * as cors from 'cors';
+import * as compression from 'compression';
+import * as express from 'express';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import { Repository } from './shared/model/repository';
 import { getApplicationConfig } from './service/app-config';
-import { ContributorsImageCache } from './service/cache-storage';
 import { fetchContributors } from './service/fetch-contributors';
+import { ContributorsImageCache } from './service/image-cache';
+import { ContributorsJsonCache } from './service/json-cache';
 import { renderContributorsImage } from './service/render-image';
+import { Repository } from './shared/model/repository';
 import { validateRepoParam } from './utils/validators';
 
 admin.initializeApp();
@@ -16,9 +18,8 @@ const bucket = admin.storage().bucket();
 const config = getApplicationConfig();
 console.debug('config', config);
 
-export const createContributorsImage = functions
-  .runWith({ timeoutSeconds: 60, memory: '1GB' })
-  .https.onRequest(async (request, response) => {
+export const createContributorsImage = functions.runWith({ timeoutSeconds: 60, memory: '1GB' }).https.onRequest(
+  express().get('*', async (request, response) => {
     const repoParam = request.query['repo'];
 
     try {
@@ -31,61 +32,74 @@ export const createContributorsImage = functions
     const repository = Repository.fromString(repoParam);
     console.debug(`repository: ${repository.toString()}`);
 
-    const imageCache = new ContributorsImageCache(bucket, { useCache: config.useCache });
+    const cache = new ContributorsImageCache(bucket, { useCache: config.useCache });
 
-    const createImage = async () => {
+    const prepare = async () => {
       console.debug('restore cache');
-      const cache = await imageCache.restore(repository);
-      if (cache) {
+      const cached = await cache.restore(repository);
+      if (cached) {
         console.debug('cache hit');
-        return cache;
+        return cached;
       }
       console.debug(`render image`);
       return renderContributorsImage(repository, { webappUrl: config.webappUrl, useHeadless: config.useHeadless });
     };
 
     try {
-      const image = await createImage();
+      const image = await prepare();
       response
         .header('Content-Type', 'image/png')
         .header('Cache-Control', 'max-age=0, no-cache')
         .status(200)
         .send(image);
       console.debug('save cache');
-      await imageCache.save(repository, image);
+      await cache.save(repository, image);
     } catch (error) {
       console.error(error);
       response.status(500).send(error);
     }
-  });
+  }),
+);
 
-const withCors = cors({ origin: true });
-export const getContributors = functions.https.onRequest((request, response) => {
-  withCors(request, response, async () => {
-    const repoParam = request.query['repo'];
+export const getContributors = functions.https.onRequest(
+  express()
+    .use(compression({}))
+    .get('*', async (request, response) => {
+      const repoParam = request.query['repo'];
 
-    try {
-      validateRepoParam(repoParam);
-    } catch (error) {
-      console.error(error);
-      response.status(400).send(error.toString());
-      return;
-    }
-    const repository = Repository.fromString(repoParam);
-    console.debug(`repository: ${repository.toString()}`);
+      try {
+        validateRepoParam(repoParam);
+      } catch (error) {
+        console.error(error);
+        response.status(400).send(error.toString());
+        return;
+      }
+      const repository = Repository.fromString(repoParam);
+      console.debug(`repository: ${repository.toString()}`);
+      const cache = new ContributorsJsonCache(bucket, { useCache: config.useCache });
 
-    try {
-      console.debug(`fetch contributors starting`);
-      const contributors = await fetchContributors(repository);
-      console.debug(`fetch contributors finished`);
+      const prepare = async () => {
+        console.debug('restore cache');
+        const cached = await cache.restore(repository);
+        if (cached) {
+          console.debug('cache hit');
+          return cached;
+        }
+        console.debug(`fetch data`);
+        return fetchContributors(repository);
+      };
 
-      response
-        .header('Content-Type', 'application/json')
-        .status(200)
-        .send(contributors);
-    } catch (err) {
-      console.error(err);
-      response.status(500).send(err.toString());
-    }
-  });
-});
+      try {
+        const contributors = await prepare();
+        response
+          .header('Content-Type', 'application/json')
+          .status(200)
+          .send(contributors);
+        console.debug('save cache');
+        await cache.save(repository, contributors);
+      } catch (err) {
+        console.error(err);
+        response.status(500).send(err.toString());
+      }
+    }),
+);
