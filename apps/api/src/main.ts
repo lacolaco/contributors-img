c0eca6c3cac2c6466f68eb9e7bbf8a48eea80b67
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-12-13 19:03:00 +0900

    fix(api): load trace-agent with require (#335)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,12 @@
-import * as tracing from '@google-cloud/trace-agent';
 import 'core-js/features/reflect';
-import { startServer } from './app';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  tracing.start();
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('@google-cloud/trace-agent').start();
 }
+
+import { startServer } from './app';
 
 startServer({
   port: process.env.PORT || 3333,
