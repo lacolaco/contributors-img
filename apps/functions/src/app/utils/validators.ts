9a748453b1cf74569668310fa986304db37d89e3
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-12-13 14:27:56 +0900

    refactor: remove functions code (#329)

--- apps/functions/src/app/utils/validators.ts
+++
@@ -1,9 +0,0 @@
-export function validateRepoParam(repoParam: unknown | null): asserts repoParam is string {
-  if (!repoParam || typeof repoParam !== 'string') {
-    throw new Error(`'repo' parameter is required.`);
-  }
-  const [, repo] = repoParam.split('/');
-  if (!repo) {
-    throw new Error(`"${repoParam}" is not a repository string`);
-  }
-}
