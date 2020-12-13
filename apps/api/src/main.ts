66021af476cfa203cea58c5ce92f0169b47999d0
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-12-13 13:03:44 +0900

    feat(api): enable in production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,11 @@
+import * as tracing from '@google-cloud/trace-agent';
 import 'core-js/features/reflect';
-
 import { startServer } from './app';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  tracing.start();
+}
 
 startServer({
   port: process.env.PORT || 3333,
