36d5a5f8e55a043c248efc3d0e08ca789205bdf1
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-04-24 06:59:00 +0900

    fix(api): use datauri for avatar image

--- apps/api/src/app/utils/svg-builder.ts
+++ apps/api/src/app/utils/svg-builder.ts
@@ -1,6 +1,7 @@
 import { Contributor } from '@lib/core';
 import * as SVG from '@svgdotjs/svg.js';
 import { JSDOM } from 'jsdom';
+import { createDataURIFromURL } from './image';
 
 declare module '@svgdotjs/svg.js' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,13 +15,17 @@ export function createSvgInstance() {
   return SVG.SVG();
 }
 
-export function createContributorAvatarImage(
+export async function createContributorAvatarImage(
   container: SVG.Container,
   { login, avatar_url, html_url }: Contributor,
   size: number,
   borderColor = '#c0c0c0',
 ) {
-  const image = container.image(avatar_url).size(size, size);
+  const imageURL = new URL(avatar_url);
+  imageURL.searchParams.set('size', size.toString());
+  const imageURI = await createDataURIFromURL(imageURL.toString());
+
+  const image = container.image(imageURI).size(size, size);
   const bg = container.pattern(size, size).add(image);
   const link = container.link(html_url).target('_blank');
   return container
