440cdeacea76a592ef6a582fb687d45d4610f75b
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-04-24 07:14:02 +0900

    fix

--- apps/api/src/app/utils/svg-builder.spec.ts
+++ apps/api/src/app/utils/svg-builder.spec.ts
@@ -11,86 +11,46 @@ describe('createSvgInstance()', () => {
 describe('createRoundedImage', () => {
   let container: Container;
 
+  const createContributor = () => ({
+    login: 'lacolaco',
+    id: 1,
+    html_url: 'https://github.com/lacolaco',
+    avatar_url: 'https://github.com/lacolaco.png',
+    contributions: 1,
+  });
+
   beforeEach(() => {
     container = createSvgInstance();
   });
 
-  it('should return svg element', () => {
-    const el = createContributorAvatarImage(
-      container,
-      {
-        login: 'foo',
-        id: 1,
-        html_url: 'a',
-        avatar_url: 'b',
-        contributions: 1,
-      },
-      64,
-    );
+  it('should return svg element', async () => {
+    const el = await createContributorAvatarImage(container, createContributor(), 64);
 
     expect(el).toBeInstanceOf(Element);
   });
 
-  it('should contain svg image', () => {
-    const el = createContributorAvatarImage(
-      container,
-      {
-        login: 'foo',
-        id: 1,
-        html_url: 'a',
-        avatar_url: 'b',
-        contributions: 1,
-      },
-      64,
-    );
+  it('should contain svg image', async () => {
+    const el = await createContributorAvatarImage(container, createContributor(), 64);
 
-    expect(el.root().findOne('image').attr('href')).toBe('b');
+    expect(el.root().findOne('image')).toBeTruthy();
   });
 
-  it('should contain svg title', () => {
-    const el = createContributorAvatarImage(
-      container,
-      {
-        login: 'foo',
-        id: 1,
-        html_url: 'a',
-        avatar_url: 'b',
-        contributions: 1,
-      },
-      64,
-    );
+  it('should contain svg title', async () => {
+    const contributor = createContributor();
+    const el = await createContributorAvatarImage(container, contributor, 64);
 
-    expect(el.findOne('title').svg()).toContain('foo');
+    expect(el.findOne('title').svg()).toContain(contributor.login);
   });
 
-  it('should contain svg link', () => {
-    const el = createContributorAvatarImage(
-      container,
-      {
-        login: 'foo',
-        id: 1,
-        html_url: 'a',
-        avatar_url: 'b',
-        contributions: 1,
-      },
-      64,
-    );
+  it('should contain svg link', async () => {
+    const contributor = createContributor();
+    const el = await createContributorAvatarImage(container, contributor, 64);
 
-    expect(el.root().findOne('a').attr('href')).toContain('a');
+    expect(el.root().findOne('a').attr('href')).toBe(contributor.html_url);
   });
 
-  it('should be sized', () => {
-    const el = createContributorAvatarImage(
-      container,
-      {
-        login: 'foo',
-        id: 1,
-        html_url: 'a',
-        avatar_url: 'b',
-        contributions: 1,
-      },
-      64,
-    );
+  it('should be sized', async () => {
+    const el = await createContributorAvatarImage(container, createContributor(), 64);
 
     expect(el.width()).toBe(64);
     expect(el.height()).toBe(64);
