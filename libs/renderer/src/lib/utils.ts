8e06fc374e935cd2d5211513871e5c75a8cd6aca
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-09-30 00:34:53 +0900

    refactor: use undici

--- libs/renderer/src/lib/utils.ts
+++ libs/renderer/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import * as SVG from '@svgdotjs/svg.js';
 import AbortController from 'abort-controller';
 import { JSDOM } from 'jsdom';
-import fetch from 'node-fetch';
+import { request } from 'undici';
 
 // Accept window from JSDOM
 declare module '@svgdotjs/svg.js' {
@@ -15,12 +15,11 @@ export async function createImageDataURI(imageUrl: string): Promise<string> {
     setTimeout(() => {
       controller.abort();
     }, 30 * 1000);
-    const resp = await fetch(imageUrl, {
-      signal: controller.signal,
-    });
-    const contentType = resp.headers.get('content-type');
-    const data = (await resp.buffer()).toString('base64');
-    return `data:${contentType};base64,${data}`;
+
+    const { headers, body } = await request(imageUrl);
+    const contentType = headers['content-type'];
+    const buf = await body.arrayBuffer().then(Buffer.from);
+    return `data:${contentType};base64,${buf.toString('base64')}`;
   } catch (error) {
     console.error(error);
     return '';
