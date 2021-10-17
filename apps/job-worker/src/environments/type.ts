781c5f4a1fc46bbad8dab4ca72f9644cdae98e0d
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-10-17 16:44:03 +0900

    chore: add job-worker project

---
+++ apps/job-worker/src/environments/type.ts
@@ -0,0 +1,5 @@
+export type Environment = {
+    production: boolean;
+    environment: string;
+  };
+  
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/libs/core', '<rootDir>/apps/webapp', '<rootDir>/apps/api', '<rootDir>/libs/renderer'],
+  projects: [
+    '<rootDir>/libs/core',
+    '<rootDir>/apps/webapp',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/renderer',
+    '<rootDir>/apps/job-worker',
+  ],
 };
