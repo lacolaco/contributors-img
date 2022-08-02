651577da023d7355ba3aaca4b2f12eee94044473
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-08-02 22:05:21 +0900

    chore: setup go development environment

---
+++ apps/worker-go/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDummy(t *testing.T) {
+	msg := "Hello, World!"
+	fmt.Println(msg)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module contrib.rocks/api-go
+
+go 1.18
