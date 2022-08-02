371b2b78b411a53f595fc67eff4062993a4159a3
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-08-03 08:46:26 +0900

    feat(worker-go): initial implementation (#1214)

--- apps/worker-go/main.go
+++ apps/worker-go/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load()
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "staging"
+	}
+	fmt.Printf("Environment: %s\n", env)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := SetEnvironment(r.Context(), env)
+		repositories, err := QueryFeaturedRepositories(ctx)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		err = SaveFeaturedRepositories(ctx, repositories, time.Now())
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		body, _ := json.MarshalIndent(repositories, "", "  ")
+		w.Write(body)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Listening on http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
