f3248faad13beb54cedf33213e4b579af9bffb2d
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-08-07 15:35:17 +0900

    refactor: move config files

--- jest.preset.js
+++
@@ -1,7 +0,0 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.base.json');
-
-module.exports = {
-  globalSetup: 'jest-preset-angular/global-setup',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: __dirname }),
-};
