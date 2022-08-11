685174e6301064793894a3150f3aeac3ebec5f5f
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-08-11 10:04:18 +0900

    fix(webapp): use the correct collection

--- apps/webapp/src/environments/environment.ts
+++ apps/webapp/src/environments/environment.ts
@@ -3,5 +3,5 @@ import { firebaseConfig } from './firebase-config';
 export const environment = {
   production: false,
   firebaseConfig: firebaseConfig.prod,
-  firestoreRootCollectionName: 'production',
+  firestoreRootCollectionName: 'development',
 };
