53cf0909d09f78f15fe67724da2f70144d9b5e41
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-09-01 22:04:39 +0900

    feat: add `anon` option (#1298)
    * feat: add `anon` option
    
    * chore: fix wrong spec
    
    * chore: fix UI

--- apps/api/internal/api/image/params_test.go
+++ apps/api/internal/api/image/params_test.go
@@ -15,6 +15,7 @@ func Test_GetImageParams_BindQuery_AllParams(t *testing.T) {
 	q.Set("repo", "angular/angular-ja")
 	q.Set("max", "100")
 	q.Set("columns", "12")
+	q.Set("anon", "1")
 	q.Set("preview", "1")
 	uri.RawQuery = q.Encode()
 	c, _ := gin.CreateTestContext(httptest.NewRecorder())
@@ -33,6 +34,9 @@ func Test_GetImageParams_BindQuery_AllParams(t *testing.T) {
 		t.Fatalf("Bound params: %v", params)
 	}
 	if params.Columns != 12 {
+		t.Fatalf("Bound params: %v", params)
+	}
+	if !params.IncludeAnonymous {
 		t.Fatalf("Bound params: %v", params)
 	}
 	if !params.Preview {
@@ -68,5 +72,43 @@ func Test_GetImageParams_BingQuery_InvalidRepository(t *testing.T) {
 
 	if err == nil {
 		t.Fatalf(err.Error())
+	}
+}
+
+func Test_GetImageParams_BingQuery_Anon_ZeroIsFalse(t *testing.T) {
+	uri := url.URL{Path: "/image"}
+	q := uri.Query()
+	q.Set("anon", "0")
+	uri.RawQuery = q.Encode()
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request, _ = http.NewRequest("GET", uri.String(), nil)
+
+	var params GetImageParams
+	err := params.bind(c)
+
+	if err == nil {
+		t.Fatalf(err.Error())
+	}
+	if params.IncludeAnonymous {
+		t.Fatalf("Bound params: %v", params)
+	}
+}
+
+func Test_GetImageParams_BingQuery_Anon_FalseIsFalse(t *testing.T) {
+	uri := url.URL{Path: "/image"}
+	q := uri.Query()
+	q.Set("anon", "false")
+	uri.RawQuery = q.Encode()
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request, _ = http.NewRequest("GET", uri.String(), nil)
+
+	var params GetImageParams
+	err := params.bind(c)
+
+	if err == nil {
+		t.Fatalf(err.Error())
+	}
+	if params.IncludeAnonymous {
+		t.Fatalf("Bound params: %v", params)
 	}
 }
