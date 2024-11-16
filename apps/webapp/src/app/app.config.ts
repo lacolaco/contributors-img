9988e6aa80d132d1354419402f6e9363ccf8d621
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-11-16 17:33:05 +0900

    fix: upgrade Angular to v16 (#1500)
    * fix: upgrade Angular to v16
    
    * chore: align related package versions

---
+++ apps/webapp/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
+import { routes } from './app-routes';
+import { provideFeaturedRepositoryDatasource } from './shared/featured-repository/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(HttpClientModule),
+    provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    provideFeaturedRepositoryDatasource(),
+  ],
+};
