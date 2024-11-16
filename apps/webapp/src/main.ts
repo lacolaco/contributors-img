dd4e6405184135f8240e6ff06428ccbfd05942d7
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-11-16 17:00:49 +0900

    fix: upgrade Angular to v16

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,30 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routes';
 import { AppComponent } from './app/app.component';
-import { provideFeaturedRepositoryDatasource } from './app/shared/featured-repository/firestore';
+import { appConfig } from './app/app.config';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideAnimations(),
-    importProvidersFrom(HttpClientModule),
-    provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
-    importProvidersFrom(provideFirestore(() => getFirestore())),
-    importProvidersFrom(provideAnalytics(() => getAnalytics())),
-    importProvidersFrom(providePerformance(() => getPerformance())),
-    provideFeaturedRepositoryDatasource(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
