7be4e339efb6f2866ac3c85fbc50b240526029c1
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-11-21 23:37:27 +0900

    chore: fix spec files

--- apps/webapp/src/app/pages/preview/header/header.component.spec.ts
+++ apps/webapp/src/app/pages/preview/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
@@ -6,11 +6,11 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HeaderComponent],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
